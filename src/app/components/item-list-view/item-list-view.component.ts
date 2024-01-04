0a3b595e8cbd34bbd3e988a480ed1e4686f9e679
mwalter <walter@mpdl.mpg.de>
2024-01-04 13:09:16 +0100

    CSS adjusted

---
+++ src/app/components/item-list-view/item-list-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pure-item-list-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-list-view.component.html',
+  styleUrl: './item-list-view.component.scss'
+})
+export class ItemListViewComponent {
+
+}
