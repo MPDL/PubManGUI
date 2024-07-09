8543a0ac03f5c4aab449bd74bde0b6eddc470b9d
Markus Haarländer <haarlaender@mpdl.mpg.de>
2024-07-09 16:12:12 +0200

    item publication state date as year in list badge

--- src/app/shared/services/pipes/date-to-year.pipe.ts
+++ src/app/shared/services/pipes/date-to-year.pipe.ts
@@ -6,9 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateToYearPipe implements PipeTransform {
 
-  transform(value: string, ...args: unknown[]): string {
+  transform(value: string | undefined, ...args: unknown[]): string | undefined {
     if (value && value.length >=4) {
-      return value.substring(0,3);
+      return value.substring(0,4);
     }
     return value;
   }
