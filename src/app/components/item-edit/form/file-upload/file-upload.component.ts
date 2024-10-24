6077a21ababd681b9a86ec1896db956db23ddec3
mwalter <walter@mpdl.mpg.de>
2024-10-24 16:53:20 +0200

    file staging started

--- src/app/components/item-edit/form/file-upload/file-upload.component.ts
+++ src/app/components/item-edit/form/file-upload/file-upload.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FileDbVO, Storage } from 'src/app/model/inge';
+import { FileDbVO, StagedFileDbVO, Storage } from 'src/app/model/inge';
 import { FileUploadDirective } from 'src/app/shared/directives/file-upload.directive';
 import { FormArray, FormGroup } from '@angular/forms';
 import { ControlType, FormBuilderService } from '../../services/form-builder.service';
+import { FileStagingService } from 'src/app/services/pubman-rest-client/file-staging.service';
 
 @Component({
   selector: 'pure-file-upload',
@@ -17,15 +18,17 @@ export class FileUploadComponent {
   @Input() file_upload_form!: FormArray<FormGroup<ControlType<FileDbVO>>>;
   
   fbs = inject(FormBuilderService);
+  fileStagingService = inject(FileStagingService);
   
   files: FileDbVO[] = [{} as FileDbVO];
   fileReader: any;
+  aaService: any;
 
   onDropFiles(files: FileDbVO[]): void {
     console.log("Event Drop Files")
     for (let file of files) {
       file.storage = Storage.INTERNAL_MANAGED;
-      this.file_upload_form.push(this.fbs.file_FG(file))
+      this.file_upload_form.push(this.fbs.file_FG(file));
     }
   }
 
@@ -43,6 +46,11 @@ export class FileUploadComponent {
         file.storage = Storage.INTERNAL_MANAGED;
         this.file_upload_form.push(this.fbs.file_FG(file))
       }
+  }
+
+  stageFile(file: FileDbVO) {
+    //let fileToStage  : StagedFileDbVO = 
+    //return this.fileStagingService.create(file, this.aaService.principal.token);
   }
 
   deleteFile(f: File){
