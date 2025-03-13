35c8d3a0a9d9b7e6bd2c0e2be8a44b4b589daced
vilarodr <vilarodriguez@mpdl.mpg.de>
2025-03-13 16:12:10 +0100

    translations update

--- src/app/components/batch/datasets/datasets.component.ts
+++ src/app/components/batch/datasets/datasets.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 
 import { BatchService } from '../services/batch.service';
 
-import { ItemListComponent} from "../../item-list/item-list.component";
+import { ItemListComponent } from "../../item-list/item-list.component";
 // import { Subject} from "rxjs";
 import { baseElasticSearchQueryBuilder } from "../../../shared/services/search-utils";
 // import { toObservable } from "@angular/core/rxjs-interop";
@@ -42,7 +42,18 @@ export default class DatasetsComponent {
       this.searchQuery.next(baseElasticSearchQueryBuilder("objectId", this.batchSvc.items));
     })
     */
-
-    this.searchQuery = of(baseElasticSearchQueryBuilder("objectId", this.batchSvc.items));
+    this.searchQuery = of({
+      bool: {
+        must: [
+          baseElasticSearchQueryBuilder("objectId", this.batchSvc.items),
+          {
+            script: {
+              script: "doc['latestVersion.versionNumber']==doc['versionNumber']"
+            }
+          }
+        ]
+      }
+    })
   }
+
 }
