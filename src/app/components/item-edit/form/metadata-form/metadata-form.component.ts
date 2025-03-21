b73a074352ff3d54b40ec87d59c512e7efda9460
mwalter <walter@mpdl.mpg.de>
2025-03-21 14:42:23 +0100

    Added 'add multiple authors' Feature

--- src/app/components/item-edit/form/metadata-form/metadata-form.component.ts
+++ src/app/components/item-edit/form/metadata-form/metadata-form.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output,  inject } from '@angula
 import { CommonModule } from '@angular/common';
 import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ControlType, FormBuilderService } from '../../services/form-builder.service';
-import { AbstractVO, AlternativeTitleVO, ContextDbVO, CreatorVO, EventVO, IdentifierVO, LegalCaseVO, MdsPublicationGenre, ProjectInfoVO, PublishingInfoVO, ReviewMethod, SourceVO, SubjectVO } from 'src/app/model/inge';
+import { AbstractVO, AlternativeTitleVO, ContextDbVO, CreatorRole, CreatorType, CreatorVO, EventVO, IdentifierVO, IdType, LegalCaseVO, MdsPublicationGenre, PersonVO, ProjectInfoVO, PublishingInfoVO, ReviewMethod, SourceVO, SubjectVO } from 'src/app/model/inge';
 import { AltTitleFormComponent } from '../alt-title-form/alt-title-form.component';
 import { CreatorFormComponent } from '../creator-form/creator-form.component';
 import { AddRemoveButtonsComponent } from '../../../../shared/components/add-remove-buttons/add-remove-buttons.component';
@@ -20,6 +20,7 @@ import { MiscellaneousService } from 'src/app/services/pubman-rest-client/miscel
 import { LoadingComponent } from 'src/app/shared/components/loading/loading.component';
 import { ContextsService } from 'src/app/services/pubman-rest-client/contexts.service';
 import { AaService } from 'src/app/services/aa.service';
+import { MessageService } from 'src/app/shared/services/message.service';
 
 @Component({
   selector: 'pure-metadata-form',
@@ -56,11 +57,15 @@ export class MetadataFormComponent implements OnInit {
   aaService = inject(AaService);
   contextService = inject(ContextsService);
   fbs = inject(FormBuilderService);
+  messageService = inject(MessageService);
   miscellaneousService = inject(MiscellaneousService);
   genreSpecificResource = this.miscellaneousService.genrePropertiesResource;
 
   allowed_genre_types = Object.keys(MdsPublicationGenre);
   review_method_types = Object.keys(ReviewMethod);
+  
+  multipleCreators = new FormControl<string>('');
+  loading: boolean = false;
 
   constructor(
     private fb: FormBuilder,
@@ -88,7 +93,6 @@ export class MetadataFormComponent implements OnInit {
   get event() {
     return this.meta_form.get('event') as FormGroup<ControlType<EventVO>>;
   }
-
   
   get identifiers() {
     return this.meta_form.get('identifiers') as FormArray<FormGroup<ControlType<IdentifierVO>>>;
@@ -134,6 +138,27 @@ export class MetadataFormComponent implements OnInit {
 
   changeGenre() {
     this.miscellaneousService.selectedGenre.set(this.meta_form.get('genre')?.value);
+  }
+
+  addMultipleCreators () {
+    console.log('Adding multiple creators');
+    this.loading = true;
+    if (this.multipleCreators?.value != null) {
+      this.miscellaneousService.getDecodedMultiplePersons(this.multipleCreators.value).subscribe(
+        (decodedCreators) => {
+          for (let creator of decodedCreators) {
+            let personVO : PersonVO = {completeName: creator.family + ', ' + creator.given, familyName: creator.family, givenName: creator.given, alternativeNames: [''], titles: [''], pseudonyms: [''], organizations: [], identifier: {id: '', type: IdType.OTHER}, orcid: ''};
+            let creatorVO : CreatorVO = {person: personVO, role: CreatorRole.AUTHOR, type: CreatorType.PERSON, organization: {identifier: '', name: ''}}; 
+            this.creators.push(this.fbs.creator_FG(creatorVO));
+          }
+          this.messageService.success('Adding multiple creators successful. Please review the list of creators.');
+          this.multipleCreators.setValue('');
+          this.loading = false;
+        }
+      );
+    } else {
+      this.messageService.error('Please enter multiple creators in the textfield.');
+    }
   }
 
   handleAltTitleNotification(event: any) {
