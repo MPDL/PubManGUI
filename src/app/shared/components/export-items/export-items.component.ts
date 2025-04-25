8f7281d76a9562eaee76d998bd17e7016ecae7b2
haarli <haarlaender@mpdl.mpg.de>
2025-04-25 10:41:33 +0200

    list exports

--- src/app/shared/components/export-items/export-items.component.ts
+++ src/app/shared/components/export-items/export-items.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewChild, ViewChildren} from '@angular/core';
 import {DefaultKeyValuePipe} from "../../services/pipes/default-key-value.pipe";
 import {citationTypes, exportTypes, ItemVersionVO} from "../../../model/inge";
 import {FormBuilder, FormControl, FormGroup, FormsModule} from "@angular/forms";
@@ -9,6 +9,13 @@ import {CslAutosuggestComponent} from "../csl-autosuggest/csl-autosuggest.compon
 import {SanitizeHtmlPipe} from "../../services/pipes/sanitize-html.pipe";
 import { environment } from 'src/environments/environment';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {ItemSelectionService} from "../../services/item-selection.service";
+import {map} from "rxjs";
+import {baseElasticSearchQueryBuilder} from "../../services/search-utils";
+import contentDisposition from "content-disposition";
+import {LoadingComponent} from "../loading/loading.component";
+
+
 
 @Component({
   selector: 'pure-export-items',
@@ -18,38 +25,68 @@ import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
     FormsModule,
     OuAutosuggestComponent,
     CslAutosuggestComponent,
-    SanitizeHtmlPipe
+    SanitizeHtmlPipe,
+    LoadingComponent
   ],
   templateUrl: './export-items.component.html',
   styleUrl: './export-items.component.scss'
 })
 export class ExportItemsComponent {
 
-  @Input() itemIds: string[] = [];
-  @Input() item: ItemVersionVO|undefined = undefined;
+  //@Input() itemIds: string[] = [];
+  //@Input() item: ItemVersionVO|undefined = undefined;
 
+  @Input() sortQuery: any;
+  //@ViewChildren(CslAutosuggestComponent) cslAutosuggestComponent!: CslAutosuggestComponent;
 
   protected readonly exportTypes = exportTypes;
   protected readonly citationTypes = citationTypes;
 
   protected restUri = environment.inge_rest_uri;
 
+  itemIds: string[] = [];
+
   selectedExportType:string = exportTypes.ENDNOTE;
   selectedCitationType:string = citationTypes.APA;
-  selectedCslId = '';
+  selectedCslId = "";
+  selectedCslName = "";
 
   currentCitation: string = '';
 
-  constructor(private itemService: ItemsService, protected activeModal: NgbActiveModal) {
+  protected loading = false;
+
+  constructor(private itemService: ItemsService, protected activeModal: NgbActiveModal, private selectionService: ItemSelectionService) {
   }
 
   ngOnInit() {
+
+    const exportType = localStorage.getItem("exportType");
+    if(exportType){
+      const expType:ExportType = JSON.parse(exportType);
+      this.selectedExportType = expType.exportType;
+      this.selectedCitationType = expType.citationType;
+      this.selectedCslId = expType.cslId;
+      this.selectedCslId = expType.cslId;
+      this.selectedCslName = expType.cslName;
+    }
+
+    this.itemIds = this.selectionService.selectedIds$.value;
     this.loadCitation();
   }
 
+  updateStoredExportInfo() {
+    const exportType: ExportType = {
+      exportType: this.selectedExportType,
+      citationType: this.selectedCitationType,
+      cslId: this.selectedCslId,
+      cslName: this.selectedCslName,
+    }
+    localStorage.setItem("exportType", JSON.stringify(exportType));
+  }
+
   loadCitation() {
-    if(!this.isFormat && (this.selectedCitationType !== citationTypes.CSL || this.selectedCslId)) {
-      this.itemService.retrieveSingleCitation(this.item?.objectId + '_' + this.item?.versionNumber, this.selectedCitationType, this.selectedCslId).subscribe(
+    if(!this.isFormat && this.itemIds.length > 0 && (this.selectedCitationType !== citationTypes.CSL || this.selectedCslId)) {
+      this.itemService.retrieveSingleCitation(this.itemIds[0], this.selectedCitationType, this.selectedCslId).subscribe(
         cit => {
           //console.log('Citation: ' +cit)
           this.currentCitation=cit;
@@ -58,26 +95,33 @@ export class ExportItemsComponent {
     }
 
   }
+
+  isValid() :boolean {
+    return this.itemIds.length > 0 &&
+      (this.selectedCitationType !== citationTypes.CSL || (this.selectedCslId?.length > 0));
+  }
   handleFormatChange($event: Event) {
     this.currentCitation = '';
     this.loadCitation();
-    console.log(this.selectedExportType)
+    this.updateStoredExportInfo();
   }
 
   handleCitationChange($event: Event) {
     this.currentCitation = '';
     this.loadCitation();
-    console.log(this.selectedExportType)
+    this.updateStoredExportInfo();
   }
 
   selectCsl(event: any) {
-    console.log(event);
     this.selectedCslId = event.id;
-    console.log(event.id);
+    this.selectedCslName = event.value;
     if(this.selectedCslId)
       this.loadCitation();
-    else
+    else {
       this.currentCitation = '';
+    }
+
+    this.updateStoredExportInfo();
   }
 
   get isFormat() {
@@ -90,9 +134,54 @@ export class ExportItemsComponent {
   }
 
   get downloadLink() {
-    return this.restUri + '/items/' + this.item?.objectId + '_' +this.item?.versionNumber
+    return this.restUri + '/items/' + this.itemIds[0]
       + '/export?format=' + this.selectedExportType
       + (this.selectedCitationType ? '&citation=' + this.selectedCitationType : '')
       + (this.selectedCslId ? '&cslConeId=' + this.selectedCslId : '')
   }
+
+  download() {
+    this.loading = true;
+    const searchQuery = {
+      query : baseElasticSearchQueryBuilder("objectId", this.itemIds),
+      size: this.itemIds.length,
+      ...this.sortQuery && {sort: [this.sortQuery]},
+    }
+
+    this.itemService.searchAndExport(searchQuery, this.selectedExportType, this.selectedCitationType, this.selectedCitationType === citationTypes.CSL ? this.selectedCslId : undefined, true).subscribe(
+      result => {
+
+        if(result.body) {
+          const blob:Blob = result.body;
+          console.log("Blob type: " + blob.type);
+          const data = window.URL.createObjectURL(blob);
+          let filename = "download"
+
+          const contentDispositionHeader = result.headers.get("Content-disposition");
+          if(contentDispositionHeader) {
+            filename = contentDisposition.parse(contentDispositionHeader).parameters['filename'];
+          }
+          const link = document.createElement('a');
+          link.href = data;
+          link.download = filename;
+          link.click();
+          window.URL.revokeObjectURL(data);
+          link.remove();
+        }
+        this.loading = false;
+
+
+
+      }
+
+    )
+  }
+}
+
+export interface ExportType {
+  exportType: string;
+  citationType: string;
+  cslId: string;
+  cslName: string;
+
 }
