9572f1cef3931710073879e81266d74ebb795b01
haarli <haarlaender@mpdl.mpg.de>
2025-09-10 17:30:34 +0200

    Enhance form validation with alternative title and subject validators, update date pattern, and adjust translations

--- src/app/directives/validation/alternative-title.validation.directive.ts
+++ src/app/directives/validation/alternative-title.validation.directive.ts
@@ -3,21 +3,19 @@ import { Errors } from "src/app/model/errors";
 import { IdType } from "src/app/model/inge";
 import { isFormValueEmpty } from "../../utils/utils";
 
-export const SubjectValidator: ValidatorFn = (control: AbstractControl,): ValidationErrors | null => {
-  const error_types = Errors;
-  const id_type = IdType;
+export const alternativeTitleValidator: ValidatorFn = (control: AbstractControl,): ValidationErrors | null => {
   let subject = control;
   if (subject !== null) {
     if (!isFormValueEmpty(subject.get('value')?.value) && isFormValueEmpty(subject.get('type')?.value)) {
-        subject.get('type')?.setErrors({[error_types.SUBJECT_TYPE_NOT_PROVIDED] : true});
+        subject.get('type')?.setErrors({[Errors.ALT_TITLE_TYPE_NOT_PROVIDED] : true});
         //return { [error_types.SUBJECT_TYPE_NOT_PROVIDED]: true };
       }
       else {
         subject.get('type')?.setErrors(null);
       }
 
-    if (isFormValueEmpty(subject.get('value')?.value) && !isFormValueEmpty(subject.get('type')?.value)) {
-      subject.get('value')?.setErrors({[error_types.SUBJECT_VALUE_NOT_PROVIDED] : true});
+    if (isFormValueEmpty(subject.get('value')?.value) && (!isFormValueEmpty(subject.get('type')?.value) || !isFormValueEmpty(subject.get('language')?.value))) {
+      subject.get('value')?.setErrors({[Errors.ALT_TITLE_VALUE_NOT_PROVIDED] : true});
       //return { [error_types.SUBJECT_TYPE_NOT_PROVIDED]: true };
     }
     else {
