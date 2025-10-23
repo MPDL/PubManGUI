3492c774b39b2abb391debd34c1f439918e04f71
vilarodr <vilarodriguez@mpdl.mpg.de>
2025-10-23 14:51:20 +0200

    INGUI-127 Import details: Released imports can't be deleted: Rollback

--- src/app/components/imports/features/logs/import-log/import-details/import-details-list.component.ts
+++ src/app/components/imports/features/logs/import-log/import-details/import-details-list.component.ts
@@ -59,7 +59,7 @@ export default class ImportDetailsListComponent implements OnInit {
   problem: number = 0;
   warning: number = 0;
 
-  released: boolean = false;
+  released: boolean = false; // INGUI-127: Flag to indicate if import has been released
 
   public filterForm: FormGroup = this.fb.group({
     fine: [true, Validators.requiredTrue],
@@ -121,15 +121,18 @@ export default class ImportDetailsListComponent implements OnInit {
                 this.fatal++;
                 break;
             }
+            /* Check if import has been released. See INGUI-127 */
+            /*
             if (this.released === false && 
               ( element.message === "import_process_release_successful" || 
                 element.message === "import_process_submit_release_successful" ||
-                element.message === "import_process_release_finished" ||  // INGUI-127 ?
-                element.message === "import_process_submit_release_finished" // INGUI-127 ?
+                element.message === "import_process_release_finished" ||  //  ?
+                element.message === "import_process_submit_release_finished" // ?
               )) {
               console.log("Import released detected in log items.");
               this.released = true;
             }
+            */
           });
 
         this.filteredLogs = this.unfilteredLogs = importsResponse;
