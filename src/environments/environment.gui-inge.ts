4ff606bdd6d86ad03bb2f52b356a56c0736177c8
haarli <haarlaender@mpdl.mpg.de>
2025-11-18 13:49:25 +0100

    Add environment configurations for "gui-inge" and "gui-inge-external"; update build script and display environment label in header

---
+++ src/environments/environment.gui-inge.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  label: "dev",
+  cone_instance_uri: "https://gui.inge.mpdl.mpg.de/cone",
+  inge_rest_uri: "https://gui.inge.mpdl.mpg.de/rest",
+  inge_uri: "https://gui.inge.mpdl.mpg.de",
+  pure_blog_feed_url: "https://blog.pure.mpg.de/json1",
+  matomo_enabled: false,
+  matomo_site_url: "",
+  matomo_site_id: ""
+};
