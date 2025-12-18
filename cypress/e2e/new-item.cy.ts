28354dbdf0cc0a403b64235dab157d6c3a75aa1a
helkv <helk@mpdl.mpg.de>
2025-12-18 11:03:13 +0100

    #INGUI-63: Make new-item a parameterized test
    - Add itemMetadataConferencePaperRequiredFields.json
    - Make new-item a parameterized test, with Testcases: Article, Conference Paper

--- cypress/e2e/new-item.cy.ts
+++ cypress/e2e/new-item.cy.ts
@@ -1,14 +1,23 @@
 describe('New Item', () => {
   const loginName = Cypress.env('testUser').loginName
   const password = Cypress.env('testUser').password
-  let itemId: string;
-  let itemData: any;
+  let itemId: string
+
+  let testData = [
+    { fixture: 'itemMetadataArticleRequiredFields', genre: 'Article', itemData: undefined as any },
+    { fixture: 'itemMetadataConferencePaperRequiredFields', genre: 'Conference Paper', itemData: undefined as any }
+  ]
+
+  before(() => {
+    testData.forEach(({fixture, genre}, index) => {
+      cy.fixture(fixture).then((data) => {
+        testData[index].itemData = data
+      })
+    })
+  })
 
   beforeEach(() => {
     cy.loginViaAPI(loginName, password)
-    cy.fixture('itemMetadataArticleRequiredFields').then((data) => {
-      itemData = data
-    })
   })
 
   afterEach(() => {
@@ -18,52 +27,56 @@ describe('New Item', () => {
     cy.logoutViaAPI()
   })
 
-  it('Create a new item via the edit page with genre Article and only required fields', () => {
-    //Given
-    cy.visit('/edit')
-    cy.intercept('POST', '/rest/items').as('createItem')
+  testData.forEach((testCase) => {
+    it(`Create a new item via the edit page with genre ${testCase.genre} and only required fields`, () => {
+      const itemData = testCase.itemData
 
-    const context = itemData.context.name
-    const title = itemData.metadata.title
-    const familyName = itemData.metadata.creators[0].person.familyName
-    const affiliation = itemData.metadata.creators[0].person.organizations[0].name
-    const dateCreated = itemData.metadata.dateCreated
-    const sourceGenre = itemData.metadata.sources[0].genre
-    const sourceTitle = itemData.metadata.sources[0].title
-    const genre = itemData.metadata.genre
+      //Given
+      cy.visit('/edit')
+      cy.intercept('POST', '/rest/items').as('createItem')
 
-    //When
-    cy.get('select[data-test="context"]').select(context)
+      const context = itemData.context.name
+      const title = itemData.metadata.title
+      const familyName = itemData.metadata.creators[0].person.familyName
+      const affiliation = itemData.metadata.creators[0].person.organizations[0].name
+      const dateCreated = itemData.metadata.dateCreated
+      const sourceGenre = itemData.metadata.sources[0].genre
+      const sourceTitle = itemData.metadata.sources[0].title
+      const genre = itemData.metadata.genre
 
-    cy.get('select[data-test="genre"]').select(genre)
-    cy.get('textarea[data-test="title"]').type(title)
+      //When
+      cy.get('select[data-test="context"]').select(context)
 
-    cy.get('#familyName').find('input').type(familyName)
-    cy.get('[data-test="add-remove-organizations"]').find('button[name="add"]').click()
-    cy.get('#personOu1').find('input').type(affiliation)
+      cy.get('select[data-test="genre"]').select(genre)
+      cy.get('textarea[data-test="title"]').type(title)
 
-    cy.get('input[data-test="date-created"]').type(dateCreated)
+      cy.get('#familyName').find('input').type(familyName)
+      cy.get('[data-test="add-remove-organizations"]').find('button[name="add"]').click()
+      cy.get('#personOu1').find('input').type(affiliation)
 
-    cy.get('#sources-metadata').click()
-    cy.get('pure-source-form #genre').select(sourceGenre)
-    cy.get('#source_title').type(sourceTitle)
+      cy.get('input[data-test="date-created"]').type(dateCreated)
 
-    cy.get('button[data-test="save"]').click()
+      cy.get('#sources-metadata').click()
+      cy.get('pure-source-form #genre').select(sourceGenre)
+      cy.get('#source_title').type(sourceTitle)
 
-    //Then
-    cy.wait('@createItem').then((interception) => {
-      // @ts-ignore
-      expect(interception.response.statusCode).to.equal(201)
-      // @ts-ignore
-      itemId = interception.response.body['objectId']
+      cy.get('button[data-test="save"]').click()
 
-      // Verify the item was created with correct metadata
-      cy.getItemViaAPI(itemId).then((response) => {
-        expect(response.body.metadata).to.deep.include(itemData.metadata)
+      //Then
+      cy.wait('@createItem').then((interception) => {
+        // @ts-ignore
+        expect(interception.response.statusCode).to.equal(201)
+        // @ts-ignore
+        itemId = interception.response.body['objectId']
+
+        // Verify the item was created with correct metadata
+        cy.getItemViaAPI(itemId).then((response) => {
+          expect(response.body.metadata).to.deep.include(itemData.metadata)
+        })
+
+        //TODO: Getting the pure-notification fails from time to time: Check when exactly the notification is displayed
+        //cy.get('pure-notification', {timeout: 10000}).should('exist')
       })
-
-      //TODO: Getting the pure-notification fails from time to time: Check when exactly the notification is displayed
-      //cy.get('pure-notification', {timeout: 10000}).should('exist')
     })
   })
 })
