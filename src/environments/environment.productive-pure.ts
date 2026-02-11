e76fcfea146c7aa9c17a9ef9480c785d53e39485
haarli <markus.haarlaender@maxit.mpg.de>
2026-02-11 11:43:57 +0100

    Add productive-pure environment configuration and build option

---
+++ src/environments/environment.productive-pure.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  label: "production",
+  cone_instance_uri: "/cone",
+  inge_rest_uri: "/rest",
+  inge_uri: "",
+  pure_blog_feed_url: "https://blog.pure.mpg.de/json1",
+  matomo_enabled: true,
+  matomo_site_url: "https://analytics.mpdl.mpg.de",
+  matomo_site_id: "1"
+};
