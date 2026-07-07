9ffc6aaa5df38e022d70417610c3b6ac40f5cea0
walterMPDL <matthias.walter@maxit.mpg.de>
2026-07-07 11:13:01 +0200

    refactor: reverted INGUI-335 - no need for trusted proxies

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,7 +5,6 @@ export const environment = {
   inge_rest_uri: "http://localhost:4200/rest",
   inge_uri: "http://localhost:4200",
   pure_blog_feed_url: "http://localhost:4200/pureblogfeed",
-  trustedProxies: "loopback",
   matomo_enabled: false,
   matomo_site_url: "",
   matomo_site_id: ""
